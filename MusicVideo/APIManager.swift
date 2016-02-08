23c9676af78d50975a3c09a7d305c70774418f09
Ned - KWC <Ned@neds-macbook-pro.local>
2016-02-08 17:17:34 +0400

    Part-4 committed

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -28,7 +28,7 @@ class APIManager {
                     //completion(result: "NSURLSession Successful!")
                     //print(data)
                     do {
-                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary {
                             print(json)
                             let priority = DISPATCH_QUEUE_PRIORITY_HIGH
                             dispatch_async(dispatch_get_global_queue(priority, 0)) {
---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Ned - KWC on 2/8/16.
+//  Copyright © 2016 Ned. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>
+
